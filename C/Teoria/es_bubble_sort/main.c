21fc60bdb8be8bf742dceb49dae2c4522141d236
Samuele Giordano <91033139+MeleGio29@users.noreply.github.com>
2021-10-04 13:26:13 +0200

    Add files via upload

--- C/Teoria/es_bubble_sort/main.c
+++ C/Teoria/es_bubble_sort/main.c
@@ -1,23 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define DIM 5
 
-void ordinaVettore(int vett[], int n){
+void leggiVettore(int vett[], int n) {
+    int k;
+
+    for(k = 0; k < n; k++) {
+        printf("Inserisci un elemento: ");
+        scanf("%d", vett + k);
+    }
+}
+
+void ordinaVettore(int *vett, int n) {
     int sup, i, a;
+    int k;
 
-    for(sup = n - 1; sup > 0; sup--){
-
-        for(k = 0; k < sup; k++){
-            if(vett[k] > vett[k+1]){
-                a = vett[k + 1];
-                vett[k + 1] = vett[k];
-                vett[k] = a;
+    for(sup = n - 1; sup > 0; sup--) {
+        for(k = 0; k < sup; k++) {
+            if(*(vett + k) > *(vett + k + 1)) {
+                a = *(vett + k + 1);
+                *(vett + k + 1) = *(vett + k);
+                *(vett + k) = a;
             }
         }
     }
 }
 
-int main()
-{
-    printf("Hello world!\n");
+void stampaVettore(int vett[], int n){
+    int k;
+
+    for(k = 0; k < n; k++){
+        printf("%d ", *(vett + k));
+    }
+}
+
+int main() {
+    int vett[DIM];
+
+    leggiVettore(vett, DIM);
+
+    ordinaVettore(vett, DIM);
+
+    printf("Vettore ordinato: ");
+
+    stampaVettore(vett, DIM);
+
+    printf("\n");
+    system("PAUSE");
     return 0;
 }

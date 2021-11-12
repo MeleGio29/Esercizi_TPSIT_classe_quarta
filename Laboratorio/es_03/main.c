68ecce2f3cf417bd93870b66cb57226f8e4ab636
Samuele Giordano <91033139+MeleGio29@users.noreply.github.com>
2021-11-12 09:00:36 +0100

    Add files via upload

---
+++ Laboratorio/es_03/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//fgets legge il file
+//strtok permette di spezzare una stringa
+
+int main()
+{
+
+    printf("\n");
+    system("PAUSE");
+    return 0;
+}
